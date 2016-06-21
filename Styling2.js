9014e1b42de278c7661f6d02150c245bb71701e7
aschnur <alexander.schnur@greenwichschools.org>
2016-06-20 23:45:23 -0400

    Update Styling2.js

--- Styling2.js
+++ Styling2.js
@@ -2,5 +2,5 @@ document.getElementById("nav01").innerHTML =	// Script to link all the buttons t
 "<ul id='menu'>" + 
 "<li> <a href='index.html'> Home </a> </li>" + 
 "<li> <a href='AboutPlayers.html'>History </a> </li>" + 
-"<li> <a href='Votenow.html'> Vote </a> </li>" + 
+"<li> <a href='votenow.html'> Vote </a> </li>" + 
 "</ul>";
